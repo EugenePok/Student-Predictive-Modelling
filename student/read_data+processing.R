893ecc4188671afbb284966f879319b036736ac6
Eugene Pok <zcpok97@gmail.com>
2019-04-08 16:32:36 +0800

    data generation edited

--- student/read_data+processing.R
+++ student/read_data+processing.R
@@ -10,4 +10,10 @@ d1$grades <- cut(d1$G3, breaks=c(-1,9,11,13,15,20),labels=c("F","D","C","B","A")
 colnames(d1)
 sapply(d1,class)
 
-
+d2.length = dim(d2)[1]
+id = 1:d2.length
+pass_fail = as.factor(d2$G3 >= 10)
+d2 = data.frame(id,d2,pass_fail)
+d2$grades <- cut(d2$G3, breaks=c(-1,9,11,13,15,20),labels=c("F","D","C","B","A"))
+colnames(d2)
+sapply(d2,class)
